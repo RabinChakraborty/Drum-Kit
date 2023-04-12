303db68689ee95b9cd7d8be30b5ee9bf7560ec76
Rabin Chakraborty <rabin747809@gmail.com>
2023-04-12 14:23:04 +0530

    modified the if statements into switch statements for the button sounds of drums

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-    // let audio = new Audio('./sounds/tom-1.mp3');
-    // audio.play();
-    if (this.innerHTML == 'w') {
-      let audio = new Audio('./sounds/tom-1.mp3');
-      audio.play();
-    }
-    if (this.innerHTML == 'a') {
-      let audio = new Audio('./sounds/tom-2.mp3');
-      audio.play();
-    }
-    if (this.innerHTML == 's') {
-      let audio = new Audio('./sounds/tom-3.mp3');
-      audio.play();
-    }
-    if (this.innerHTML == 'd') {
-      let audio = new Audio('./sounds/tom-4.mp3');
-      audio.play();
-    }
-    if (this.innerHTML == 'j') {
-      let audio = new Audio('./sounds/snare.mp3');
-      audio.play();
-    }
-    if (this.innerHTML == 'k') {
-      let audio = new Audio('./sounds/crash.mp3');
-      audio.play();
-    }
-    if (this.innerHTML == 'l') {
-      let audio = new Audio('./sounds/kick-bass.mp3');
-      audio.play();
+    let button = this.innerHTML;
+    let audio = undefined;
+    switch (button) {
+      case 'w':
+        audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
+        break;
+      case 'a':
+        audio = new Audio('./sounds/tom-2.mp3');
+        audio.play();
+        break;
+      case 's':
+        audio = new Audio('./sounds/tom-3.mp3');
+        audio.play();
+        break;
+      case 'd':
+        audio = new Audio('./sounds/tom-4.mp3');
+        audio.play();
+        break;
+      case 'j':
+        audio = new Audio('./sounds/snare.mp3');
+        audio.play();
+        break;
+      case 'k':
+        audio = new Audio('./sounds/crash.mp3');
+        audio.play();
+        break;
+      case 'l':
+        audio = new Audio('./sounds/kick-bass.mp3');
+        audio.play();
+        break;
+      default:
+        break;
     }
   });
 }
