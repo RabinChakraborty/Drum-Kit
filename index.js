764cd2e21e5635c76516e514c48c4f436689be7b
Rabin Chakraborty <rabin747809@gmail.com>
2023-04-12 06:32:58 +0530

    Added sounds to the drum

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-alert('hello');
+for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    // let audio = new Audio('./sounds/tom-1.mp3');
+    // audio.play();
+    if (this.innerHTML == 'w') {
+      let audio = new Audio('./sounds/tom-1.mp3');
+      audio.play();
+    }
+    if (this.innerHTML == 'a') {
+      let audio = new Audio('./sounds/tom-2.mp3');
+      audio.play();
+    }
+    if (this.innerHTML == 's') {
+      let audio = new Audio('./sounds/tom-3.mp3');
+      audio.play();
+    }
+    if (this.innerHTML == 'd') {
+      let audio = new Audio('./sounds/tom-4.mp3');
+      audio.play();
+    }
+    if (this.innerHTML == 'j') {
+      let audio = new Audio('./sounds/snare.mp3');
+      audio.play();
+    }
+    if (this.innerHTML == 'k') {
+      let audio = new Audio('./sounds/crash.mp3');
+      audio.play();
+    }
+    if (this.innerHTML == 'l') {
+      let audio = new Audio('./sounds/kick-bass.mp3');
+      audio.play();
+    }
+  });
+}
