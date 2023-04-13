67258f66973412fcfd6722a217f96ce6af7b4d95
Rabin Chakraborty <rabin747809@gmail.com>
2023-04-13 14:59:58 +0530

    Added keydown event so that when pressed the right key the drums play

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
+//button press
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
     let button = this.innerHTML;
-    let audio = undefined;
-    switch (button) {
-      case 'w':
-        audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
-        break;
-      case 'a':
-        audio = new Audio('./sounds/tom-2.mp3');
-        audio.play();
-        break;
-      case 's':
-        audio = new Audio('./sounds/tom-3.mp3');
-        audio.play();
-        break;
-      case 'd':
-        audio = new Audio('./sounds/tom-4.mp3');
-        audio.play();
-        break;
-      case 'j':
-        audio = new Audio('./sounds/snare.mp3');
-        audio.play();
-        break;
-      case 'k':
-        audio = new Audio('./sounds/crash.mp3');
-        audio.play();
-        break;
-      case 'l':
-        audio = new Audio('./sounds/kick-bass.mp3');
-        audio.play();
-        break;
-      default:
-        break;
-    }
+    makeSound(button);
   });
+}
+//keyboard press
+document.addEventListener('keydown', function (e) {
+  makeSound(e.key);
+});
+//the function that plays the sound.
+function makeSound(key) {
+  let audio = undefined;
+  switch (key) {
+    case 'w':
+      audio = new Audio('./sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case 'a':
+      audio = new Audio('./sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 's':
+      audio = new Audio('./sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case 'd':
+      audio = new Audio('./sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case 'j':
+      audio = new Audio('./sounds/snare.mp3');
+      audio.play();
+      break;
+    case 'k':
+      audio = new Audio('./sounds/crash.mp3');
+      audio.play();
+      break;
+    case 'l':
+      audio = new Audio('./sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    default:
+      break;
+  }
 }
